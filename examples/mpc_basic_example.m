76c1380c62a81cf60730f872312e04ad1ab46e2c
Alexander Domahidi <domahidi@control.ee.ethz.ch>
2016-09-23 11:44:44 +0200

    added a fully functional basic mpc example

---
+++ examples/mpc_basic_example.m
@@ -0,0 +1,95 @@
+% Basic MPC example demonstrating the use of Yalmip to formulate MPC 
+% problems and FORCES to solve them very quickly.
+
+% Simple MPC - double integrator example for use with FORCES Pro
+% 
+%  min   xN'*P*xN + sum_{i=1}^{N-1} xi'*Q*xi + ui'*R*ui
+% xi,ui
+%       s.t. x1 = x
+%            x_i+1 = A*xi + B*ui  for i = 1...N-1
+%            xmin <= xi <= xmax   for i = 1...N
+%            umin <= ui <= umax   for i = 1...N
+%
+% and P is solution of Ricatti eqn. from LQR problem
+
+clear; clc;
+
+%% MPC problem data
+
+% system matrices
+A = [1.1 1; 0 1];
+B = [1; 0.5];
+[nx,nu] = size(B);
+
+% horizon
+N = 10;
+
+% cost matrics
+Q = eye(2);
+R = eye(1);
+[~,P] = dlqr(A,B,Q,R);
+
+% constraints
+umin = -0.5;     umax = 0.5;
+xmin = [-5; -5]; xmax = [5; 5];
+
+%% Build MPC problem in Yalmip
+
+% Initialize objective and constraints of the problem
+cost = 0;
+const = [];
+
+% Cell arrays for x_0, x_1, ..., x_N and u_0, ..., u_N-1
+x = num2cell(sdpvar(nx,N+1),1);
+u = num2cell(sdpvar(nu,N),1);
+
+for i = 1:N        
+    % cost
+    if( i == N )
+        cost = cost + 0.5*x{i+1}'*P*x{i+1} + 0.5*u{i}'*R*u{i};
+    else
+        cost = cost + 0.5*x{i+1}'*Q*x{i+1} + 0.5*u{i}'*R*u{i};
+    end
+
+    % model
+    const = [const, x{i+1} == A*x{i} + B*u{i}];
+
+    % bounds
+    const = [const, umin <= u{i} <= umax];
+    const = [const, xmin <= x{i+1} <= xmax]; 
+end
+
+
+%% Create controller object (generates code)
+codeoptions = getOptions('FORCESsolver');
+controller = optimizerFORCES(const, cost, codeoptions, x{1}, u{1});
+
+
+%% Simulate
+x1 = [-4; 2];
+kmax = 30;
+X = zeros(nx,kmax+1); X(:,1) = x1;
+U = zeros(nu,kmax);
+problem.z1 = zeros(2*nx,1);
+for k = 1:kmax
+    %problem.minusA_times_x0 = -A*X(:,k);
+    %eval(sprintf('[solverout,exitflag,info] = %s(problem);', codeoptions.name));
+    [U(:,k),exitflag,info] = controller{ X(:,k) };
+    if( exitflag == 1 )
+        solvetime(k) = info.solvetime;
+    else
+        info
+        error('Some problem in solver');
+    end
+    X(:,k+1) = A*X(:,k) + B*U(:,k);
+end
+
+
+%% plot
+figure(1); clf;
+subplot(2,1,1); grid on; title('states'); hold on;
+plot([1 kmax], [xmax xmax]', 'r--'); plot([1 kmax], [xmin xmin]', 'r--');
+ylim(1.1*[min(xmin),max(xmax)]); stairs(1:kmax,X(:,1:kmax)');
+subplot(2,1,2);  grid on; title('input'); hold on;
+plot([1 kmax], [umax umax]', 'r--'); plot([1 kmax], [umin umin]', 'r--');
+ylim(1.1*[min(umin),max(umax)]); stairs(1:kmax,U(:,1:kmax)');
